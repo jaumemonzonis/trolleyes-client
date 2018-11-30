9fb133776cceede236801eca520fb44cc4d9fa89
jaume monzonis <jaume monzonis@LAPTOP-53UQI7G0>
2018-11-30 12:18:48 +0100

    carrito inicial

---
+++ public_html/js/app/carrito/carrito.js
@@ -0,0 +1,106 @@
+'use strict'
+
+moduleCarrito.controller('carritoCarritoController', ['$scope', '$http', '$location', 'toolService', '$routeParams', 'sessionService',"$window",
+    function ($scope, $http, $location, toolService, $routeParams, oSessionService, $window) {
+
+       
+
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=show'
+        }).then(function (response) {
+            $scope.status = response.status;
+            $scope.ajaxData = response.data.message;
+        }, function (response) {
+            $scope.status = response.status;
+            $scope.ajaxData = response.data.message || 'Request failed';
+        });
+
+        function show() {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=show'
+            }).then(function (response) {
+                $scope.status = response.status;
+                $scope.ajaxData = response.data.message;
+            }, function (response) {
+                $scope.status = response.status;
+                $scope.ajaxData = response.data.message || 'Request failed';
+            });
+        }
+
+
+
+        $scope.add = function (id) {
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=add&prod=' + id
+            }).then(function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataAdd = response.data.message;
+//                for(var i=0;i<$scope.ajaxDataAdd.length;i++){
+//                    if($scope.ajaxDataAdd[i]['obj_producto'][id]===id){
+//                       $scope.cantidad= $scope.ajaxDataAdd[i]['cantidad'];
+//                    }
+//                }              
+            }, function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataAdd = response.data.message || 'Request failed';
+            });
+        };
+
+        $scope.reduce = function (id) {
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=reduce&prod=' + id
+            }).then(function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataReduce = response.data.message;
+//                for(var i=0;i<$scope.ajaxDataReduce.length;i++){
+//                    if($scope.ajaxDataReduce[i]['obj_producto'][id]===id){
+//                       $scope.cantidad= $scope.ajaxDataReduce[i]['cantidad'];
+//                    }
+//                }   
+            }, function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataReduce = response.data.message || 'Request failed';
+            });
+        };
+
+        $scope.empty = function () {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=empty'
+            }).then(function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataEmpty = response.data.message;
+            
+                 show();
+            }, function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataEmpty = response.data.message || 'Request failed';
+            });
+            
+        };
+
+
+        if (oSessionService.getUserName() !== "") {
+            $scope.loggeduser = oSessionService.getUserName();
+            $scope.loggeduserid = oSessionService.getId();
+            $scope.logged = true;
+        }
+
+        $scope.isActive = toolService.isActive;
+
+    $scope.volver = function () {
+            $window.history.back();
+        }
+
+    }
+
+
+
+]);
---
+++ public_html/js/app/carrito/plist.js
@@ -0,0 +1,161 @@
+'use strict'
+
+moduleCarrito.controller('carritoPlistController', ['$scope', '$http', '$location', 'toolService', '$routeParams', 'sessionService',
+    function ($scope, $http, $location, toolService, $routeParams, oSessionService) {
+
+        $scope.totalPages = 1;
+        $scope.conectado = false; 
+        $scope.ajaxDataAdd=0;
+        $scope.ajaxDataReduce=0;
+        
+        if (!$routeParams.order) {
+            $scope.orderURLServidor = "";
+            $scope.orderURLCliente = "";
+        } else {
+            $scope.orderURLServidor = "&order=" + $routeParams.order;
+            $scope.orderURLCliente = $routeParams.order;
+        }
+
+        if (!$routeParams.rpp) {
+            $scope.rpp = 10;
+        } else {
+            $scope.rpp = $routeParams.rpp;
+        }
+
+        if (!$routeParams.page) {
+            $scope.page = 1;
+        } else {
+            if ($routeParams.page >= 1) {
+                $scope.page = $routeParams.page;
+            } else {
+                $scope.page = 1;
+            }
+        }
+        
+        $scope.add = function (id) {            
+            
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=add&prod='+id
+            }).then(function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataAdd = response.data.message; 
+                $scope.cantidad = response.data.message.obj_tipoProducto.cantidad;
+                
+                
+            }, function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataAdd = response.data.message || 'Request failed';
+            });
+        };
+        
+        $scope.reduce = function (id) {            
+            
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/trolleyes/json?ob=carrito&op=reduce&prod='+id
+            }).then(function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataReduce = response.data.message;
+//                for(var i=0;i<$scope.ajaxDataReduce.length;i++){
+//                    if($scope.ajaxDataReduce[i]['obj_producto'][id]===id){
+//                       $scope.cantidad= $scope.ajaxDataReduce[i]['cantidad'];
+//                    }
+//                }   
+            }, function (response) {
+                $scope.status = response.status;
+                $scope.ajaxDataReduce = response.data.message || 'Request failed';
+            });
+        };
+
+
+        $scope.resetOrder = function () {
+            $location.url(`carrito/plist/` + $scope.rpp + `/` + $scope.page);
+        }
+
+
+        $scope.ordena = function (order, align) {
+            if ($scope.orderURLServidor == "") {
+                $scope.orderURLServidor = "&order=" + order + "," + align;
+                $scope.orderURLCliente = order + "," + align;
+            } else {
+                $scope.orderURLServidor = $scope.orderURLServidor + "-" + order + "," + align;
+                $scope.orderURLCliente = $scope.orderURLCliente + "-" + order + "," + align;
+            }
+            $location.url(`producto/plist/` + $scope.rpp + `/` + $scope.page + `/` + $scope.orderURLCliente);
+        }
+        
+        //getcount
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/trolleyes/json?ob=producto&op=getcount'
+        }).then(function (response) {
+            $scope.status = response.status;
+            $scope.ajaxDataNum = response.data.message;
+            $scope.totalPages = Math.ceil($scope.ajaxDataNum / $scope.rpp);
+            if ($scope.page > $scope.totalPages) {
+                $scope.page = $scope.totalPages;
+                $scope.update();
+            }
+            pagination2();
+        }, function (response) {
+            $scope.ajaxDataNum = response.data.message || 'Request failed';
+            $scope.status = response.status;
+        });
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/trolleyes/json?ob=producto&op=getpage&rpp=' + $scope.rpp + '&page=' + $scope.page + $scope.orderURLServidor
+        }).then(function (response) {
+            $scope.status = response.status;
+            $scope.ajaxData = response.data.message;
+        }, function (response) {
+            $scope.status = response.status;
+            $scope.ajaxData = response.data.message || 'Request failed';
+        });
+
+
+
+        $scope.update = function () {
+            $location.url(`producto/plist/` + $scope.rpp + `/` + $scope.page + '/' + $scope.orderURLCliente);
+        };
+
+
+        $scope.carrito = function () {
+            $location.url(`carrito/carrito`);
+        };
+
+        //paginacion neighbourhood
+        function pagination2() {
+            $scope.list2 = [];
+            $scope.neighborhood = 3;
+            for (var i = 1; i <= $scope.totalPages; i++) {
+                if (i === $scope.page) {
+                    $scope.list2.push(i);
+                } else if (i <= $scope.page && i >= ($scope.page - $scope.neighborhood)) {
+                    $scope.list2.push(i);
+                } else if (i >= $scope.page && i <= ($scope.page - -$scope.neighborhood)) {
+                    $scope.list2.push(i);
+                } else if (i === ($scope.page - $scope.neighborhood) - 1) {
+                    $scope.list2.push("...");
+                } else if (i === ($scope.page - -$scope.neighborhood) + 1) {
+                    $scope.list2.push("...");
+                }
+            }
+        }
+
+       if (oSessionService.getUserName() !== "") {
+            $scope.loggeduser = oSessionService.getUserName();
+            $scope.loggeduserid = oSessionService.getId();
+            $scope.logged = true;
+        }
+
+        $scope.isActive = toolService.isActive;
+
+
+
+    }
+
+
+
+]);
